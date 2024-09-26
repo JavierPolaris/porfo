f74342914b2741a113c8617900937781679ef420
jcantonSilbon <jcanton@silbon.es>
2024-09-26 18:35:47 +0200

    back

---
+++ bakend/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const app = express();
+
+// Middleware
+app.use(cors()); // Permitir solicitudes desde cualquier origen (útil para conectar con tu frontend)
+app.use(express.json()); // Para analizar el cuerpo de las solicitudes JSON
+
+// Ruta para enviar correos
+app.post('/api/send-email', (req, res) => {
+  const { email, nombre, mensaje } = req.body;
+
+  // Configuración de Nodemailer
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'ktmdemonia@gmail.com',
+      pass: 'Afrotuko1'
+    }
+  });
+
+  let mailOptions = {
+    from: email,
+    to: 'ktmdemonia@gmail.com',
+    subject: `Nuevo mensaje de ${nombre}`,
+    text: mensaje
+  };
+
+  // Enviar el correo
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Error al enviar el correo');
+    } else {
+      console.log('Correo enviado: ' + info.response);
+      res.status(200).send('Correo enviado correctamente');
+    }
+  });
+});
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
